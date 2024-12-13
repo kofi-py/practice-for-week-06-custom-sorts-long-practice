353fea0af80254e4dd227238c3d933406cbff38f
Kofi <pastorkofi101@gmail.com>
2024-12-13 05:28:47 -0500

    Do exercise for App Academy

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,27 +1,49 @@
 function ageSort(users) {
-  // Your code here
+  return users.sort((a, b) => a.age - b.age)
 }
 
 function oddEvenSort(arr) {
-  // Your code here
+  return arr.sort((a, b) => {
+    if (a % 2 === 0 && b % 2 !== 0) {
+      return 1
+    } else if (a % 2 !== 0 && b % 2 === 0) {
+      return -1
+    } else {
+      return a - b
+    }
+  })
 }
 
 function validAnagrams(s, t) {
-  // Your code here
+  if (s.length !== t.length) return false
+  const sortString = (str) => str.split('').sort().join('')
+  return sortString(s) === sortString(t)
 }
 
 function reverseBaseSort(arr) {
-  // Your code here
+  return arr.sort((a, b) => {
+    const aLength = a.toString().length
+    const bLength = b.toString().length
+    if (aLength === bLength) return a - b // Ascending within group
+    return bLength - aLength // Descending by digit count
+  })
 }
 
 function frequencySort(arr) {
-  // Your code here
-}
+  const freqMap = arr.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1
+    return acc
+  }, {})
 
+  return arr.sort((a, b) => {
+    if (freqMap[a] === freqMap[b]) return b - a // Descending for same frequency
+    return freqMap[a] - freqMap[b] // Ascending by frequency
+  })
+}
 module.exports = [
   oddEvenSort,
   validAnagrams,
   reverseBaseSort,
   frequencySort,
   ageSort,
-];
+]
